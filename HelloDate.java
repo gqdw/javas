3b3beef0fd42ca95379750d6f209744bf7215d77
gqdw <aca_jingru@qq.com>
2016-09-09 08:00:48 +0800

    add static value

--- HelloDate.java
+++ HelloDate.java
@@ -1,9 +1,16 @@
 import java.util.*;
+class StaticTest{
+	static int i = 47;
+}
 
 public class HelloDate{
 	public static void main(String[] args){
 		System.out.println("hello, it's: ");
 		System.out.println(new Date());
+		StaticTest st1 = new StaticTest();
+		StaticTest st2 = new StaticTest();
+		System.out.println(st1.i);
+		System.out.println(st2.i);
 }
 }
 
