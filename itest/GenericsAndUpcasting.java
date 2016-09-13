b6afdf5069791328de8861c8964f6ec86d53b3f0
gqdw <aca_jingru@qq.com>
2016-09-13 08:07:37 +0800

    add toString() and changeto List

--- itest/GenericsAndUpcasting.java
+++ itest/GenericsAndUpcasting.java
@@ -2,7 +2,11 @@ import java.util.*;
 
 class Apple {}
 
-class GrannySmith extends Apple {}
+class GrannySmith extends Apple {
+	public String toString(){
+		return "GrannySmith abc";
+	}
+}
 
 class Gala extends Apple {}
 
@@ -12,7 +16,8 @@ class Braeburn extends Apple {}
 
 public class GenericsAndUpcasting {
 	public static void main(String[] args) {
-		ArrayList<Apple> apples = new ArrayList<Apple>();
+		List<Apple> apples = new ArrayList<Apple>();
+		// ArrayList<Apple> apples = new ArrayList<Apple>();
 		apples.add(new GrannySmith());
 		apples.add(new Gala());
 		apples.add(new Fuji());
