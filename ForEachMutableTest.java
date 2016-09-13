a443b83086424cea4954e095678da2f8e2417fc1
gqdw <aca_jingru@qq.com>
2016-09-13 17:59:52 +0800

    foreach

---
+++ ForEachMutableTest.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+ 
+public class ForEachMutableTest {
+   public static void main(String[] args) {
+      List<StringBuilder> lst = new ArrayList<StringBuilder>();
+      lst.add(new StringBuilder("alpha"));
+      lst.add(new StringBuilder("beta"));
+      lst.add(new StringBuilder("charlie"));
+      System.out.println(lst);   // [alpha, beta, charlie]
+ 
+      for (StringBuilder sb : lst) {
+         sb.append("88");   // can modify "mutable" objects
+         System.out.println(sb.hashCode());
+      }
+      System.out.println(lst);  // [alpha88, beta88, charlie88]
+   }
+}
