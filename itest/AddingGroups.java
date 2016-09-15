af4caf92b1a5f341fd92eec071309cdfa2d7fbd0
gqdw <aca_jingru@qq.com>
2016-09-15 18:41:36 +0800

    new files

---
+++ itest/AddingGroups.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class AddingGroups{
+	public static void main(String[] args) {
+		// int[] a = {1,2,3,4,5};
+		Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+		Integer[] moreInts = {6,7,8,9,10};
+	}
+}
---
+++ itest/ArrayClassObj.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ArrayClassObj{
+	public static void main(String[] args) {
+		Random rand = new Random(47);
+		Integer[] a = new Integer[rand.nextInt(20)];
+		System.out.println("length of a = " + a.length);
+
+		for(int i = 0; i < a.length; i++)
+			a[i] = rand.nextInt(500);
+		System.out.println(Arrays.toString(a));
+	}
+}
