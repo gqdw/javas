d02acc38ba89f58ade98ac115c9fa64aecead69a
gqdw <aca_jingru@qq.com>
2016-10-25 14:18:47 +0800

    Changes to be committed: 	new file:   ApplesAndOrangesWithGenerics.java 	apples with Generics

---
+++ holding/ApplesAndOrangesWithGenerics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ApplesAndOrangesWithGenerics {
+	public static void main(String[] args){
+		ArrayList<Apple> apples = new ArrayList<Apple>();
+		for(int i =0; i < 3; i++)
+			apples.add(new Apple());
+
+		// apples.add(new Orange());
+		for(int i =0; i < apples.size(); i++)
+			System.out.println(apples.get(i).id());
+	}
+}
