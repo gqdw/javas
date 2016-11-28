0eb73609f92548c0dcbf05ce70b4019536ad6e2e
gqdw <aca_jingru@qq.com>
2016-11-28 14:12:49 +0800

    Changes to be committed: 	new file:   ForEachFloat.java 	for(float x:f)

---
+++ control/ForEachFloat.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+public class ForEachFloat{
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		float f[] = new float[10];
+		for(int i = 0;i < 10; i++)
+			f[i] = rand.nextFloat();
+		for(float x: f)
+			System.out.println(x);
+	}
+}
