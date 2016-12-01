f964931e86308a60b6d58b6b2b32f1700fc71da4
gqdw <aca_jingru@qq.com>
2016-12-01 10:24:22 +0800

    Changes to be committed: 	new file:   BandPass.java 	new file:   Filter.java 	new file:   HighPass.java 	new file:   LowPass.java 	new file:   Waveform.java

---
+++ interfaces/filters/Filter.java
@@ -0,0 +1,9 @@
+
+public class Filter {
+	public String name(){
+		return getClass().getSimpleName();
+	}
+	public Waveform process(Waveform input){
+		return input;
+	}
+}
---
+++ interfaces/filters/Waveform.java
@@ -0,0 +1,8 @@
+
+public class Waveform {
+	private static long counter;
+	private final long id = counter++;
+	public String toString(){
+		return "Waveform " + id;
+	}
+}
