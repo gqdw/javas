df0c1f83aa1d813c966def56b7b8f87122d8ef90
gqdw <aca_jingru@qq.com>
2016-12-06 18:41:14 +0800

    Changes to be committed: 	new file:   MultidimensionalPrimitiveArray.java 	多维数组，打印用Arrays.deepToString()

---
+++ arrays/MultidimensionalPrimitiveArray.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class MultidimensionalPrimitiveArray {
+	public static void main(String[] args) {
+		int[][] a = {
+			{1 ,2 ,3},
+			{4, 5, 6},
+		};
+
+		System.out.println(Arrays.deepToString(a));
+		System.out.println(Arrays.toString(a));
+	}
+}
