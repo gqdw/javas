d9965104a225bc4aa3c9ffc1288cd2eaaf93984d
gqdw <aca_jingru@qq.com>
2016-12-06 15:17:29 +0800

    Changes to be committed: 	new file:   ClassInitialization.java 	Class initable = Initable.class;

---
+++ typeinfo/ClassInitialization.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Initable {
+	static final int staticFinal = 47;
+	static final int staticFinal2 = ClassInitialization.rand.nextInt(1000);
+	static{
+		System.out.println("Initializing Initable");
+	}
+}
+
+class Initable2 {
+	static int staticNonFinal = 147;
+	static {
+		System.out.println("Initializing Initable2");
+	}
+}
+
+class Initable3 {
+	static int staticNonFinal = 74;
+	static {
+		System.out.println("Initializing Initable3");
+	}
+}
+
+public class ClassInitialization{
+	public static Random rand = new Random();
+	public static void main(String[] args) {
+		Class initable = Initable.class;
+		System.out.println("after creating initable ref");
+		System.out.println(Initable.staticFinal);
+
+		System.out.println(Initable.staticFinal2);
+
+		System.out.println(Initable2.staticNonFinal);
+		try {
+			Class initable3 = Class.forName("Initable3");
+		} catch(ClassNotFoundException e){
+			System.out.println("cannot found class");
+		}	
+		System.out.println("after creating Initable3 ref");
+		System.out.println(Initable3.staticNonFinal);
+	}
+}
