d0dc4cba1f1791079db3f973447b0f20cc477944
gqdw <aca_jingru@qq.com>
2016-12-07 14:22:32 +0800

    Changes to be committed: 	new file:   sortTest.java 	Arrays.sort()

---
+++ arrays/sortTest.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+
+public class sortTest{
+	public static void main(String[] args) {
+		int[] a = new int[]{3, 1, -9, 4};
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+	}
+}
